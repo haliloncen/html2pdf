7583351e9868232d1f5e80dd28b4859d23dcab62
Oğuzhan <cfmelun@gmail.com>
2021-11-04 11:50:06 +0300

    add args for docker sandbox

--- src/html2pdf.ts
+++ src/html2pdf.ts
@@ -10,25 +10,25 @@ export interface Options {
 
 /**
  * Create PDF from HTML content
- * 
+ *
  * @param {string} html HTML string
  * @param {Options} options PDF sizes & margin in px unit
- * 
+ *
  * example
  * import { createPDF } from '@haliloncen/html2pdf'
- * 
+ *
  * const html = '<h1>Hello World</h1>'
  * createPDF(html)
  *  .then(buffer => ...)
  *  .catch(err => ...)
- * 
+ *
  * async function() {
  *  const buffer = await createPDF(html, { window: { width: '1920px', height: '1080px' }, margin: '32px' })
  * }
- * 
+ *
  */
 export async function createPDF(html: string, options: Options = { window: { width: '1920px', height: '1080px' }, margin: '32px' }) {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
   const page = await browser.newPage()
   await page.setContent(html, { waitUntil: 'networkidle0' })
 
